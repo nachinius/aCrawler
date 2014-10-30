af52ca589b1f5fc62c2edeb9d9d6c4ab44f428b1
nachinius <ignacio@streamlinesocial.com>
2014-10-30 10:49:55 -0300

    add cache structure

---
+++ src/Nachinius/Command/Components/Cache.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nachinius\Command\Components;
+
+class Cache {
+    
+    private $dir = __DIR__;
+    
+    public function __construct($dir) {
+        
+    }
+    
+    public function set($key, $content) {
+        
+    }
+    
+    public function get($key) {
+        
+    }
+}
---
+++ src/Nachinius/Command/Tests/Components/CacheTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use \PHPUnit_Framework_TestCase;
+use Nachinius\Command\Components\Cache;
+
+class CacheTest extends PHPUnit_Framework_TestCase {
+    
+    public function testSetter() {
+        
+    }
+    
+    public function testGetter() {
+        
+    }
+}
