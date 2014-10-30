1377813bae0110356f07a4b7d25c2ccf87afef8f
nachinius <ignacio@streamlinesocial.com>
2014-10-30 16:59:18 -0300

    test cache with vfsStream

--- src/Nachinius/Command/Components/Cache.php
+++ src/Nachinius/Command/Components/Cache.php
@@ -2,19 +2,48 @@
 
 namespace Nachinius\Command\Components;
 
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Cache to the filesystem, one key per filename.
+ * 
+ * @author ignacio
+ *
+ */
 class Cache {
     
-    private $dir = __DIR__;
+    private $workspace = NULL;
     
+    /**
+     * 
+     * @param string $dir
+     */
     public function __construct($dir) {
+        $this->workspace = $dir;
+    }
+    
+    /**
+     * Create the directory in which the cache
+     * is going to be stored, if not present.
+     */
+    public function prepare() {
+        if (file_exists($this->workspace) === false) {
+            mkdir($this->workspace, 0700, true);
+        }
+    }
+    
+    /**
+     * Transform the $key to a filename
+     * 
+     * @param string $key
+     */
+    public function keyToFilename($key) {
         
     }
     
     public function set($key, $content) {
-        
     }
     
     public function get($key) {
-        
     }
 }
--- src/Nachinius/Command/Tests/Components/CacheTest.php
+++ src/Nachinius/Command/Tests/Components/CacheTest.php
@@ -1,15 +1,46 @@
 <?php
-
 use \PHPUnit_Framework_TestCase;
 use Nachinius\Command\Components\Cache;
+use org\bovigo\vfs\vfsStream;
 
-class CacheTest extends PHPUnit_Framework_TestCase {
-    
-    public function testSetter() {
-        
+class CacheTest extends PHPUnit_Framework_TestCase
+{
+
+    private $root;
+    private $dir;
+    private $cache;
+
+    public function setUp()
+    {
+        $this->root = vfsStream::setup('root');
+        $this->dir = vfsStream::url('root/data');
+        $this->cache = new Cache($this->dir);
     }
     
-    public function testGetter() {
+    public function tearDown()
+    {
+        $this->root = NULL;
+        $this->dir = NULL;
+        $this->cache = NULL;
+    }
+
+    public function testPrepare()
+    {
+        // precheck
+        $this->assertFalse($this->root->hasChild('data'), 'incorrect setUp');
+        
+        // test
+        $this->cache->prepare();
+
+        // check
+        $this->assertTrue($this->root->hasChild('data'), 'directory not found');
+    }
+    
+    public function testSetAndGet() {
+        
+        $cache = $this->cache;
+        $cache->prepare();
+        
         
     }
 }
