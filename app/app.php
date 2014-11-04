735c382c4bcda82b3f07126c99277038078e124a
Ignacio Peixoto <ignacio.peixoto@gmail.com>
2014-11-04 20:55:32 -0300

    use configuration files for DI

--- app/app.php
+++ app/app.php
@@ -4,23 +4,14 @@ require_once __DIR__ . '/bootstrap.php';
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 // Using container to configure
 
 $container = new ContainerBuilder();
-
-// cache
-$container->register('fs', 'Symfony\\Component\\Filesystem\\Filesystem');
-$container->register('cache', 'Nachinius\\Command\\Components\\Cache')
-    ->addArgument(__DIR__ . '/../data')
-    ->addArgument(new Reference('fs'));
-
-// Commands as services
-$container->register('LocationCommand', 'Nachinius\\Command\\LocationCommand')->addArgument(new Reference('cache'));
-
-// app as service
-$container->register('app','Symfony\\Component\\Console\\Application')
-// register the command in the app
-->addMethodCall('add',array(new Reference('LocationCommand')));
+$container->setParameter('app.root_dir', __DIR__);
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
+$loader->load('services.yml');
 
 $application = $container->get('app')->run();
